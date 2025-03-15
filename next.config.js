9e20df6321fee76723112dbf9711149c06f5f828
Ege Önder <40398628+EgeOnder@users.noreply.github.com>
2025-03-15 21:01:39 +0100

    fix: remove gh-pages

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: false,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default config;
