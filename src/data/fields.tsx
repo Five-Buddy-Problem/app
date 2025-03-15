78bdde6dda2a61d74d249d94cea202a1137c7868
Ege Önder <40398628+EgeOnder@users.noreply.github.com>
2025-03-15 20:48:43 +0100

    fix: build errors

--- src/data/fields.tsx
+++ src/data/fields.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
 "use client";
 
 import type { GeoJSONData } from "@/components/world/globe";
